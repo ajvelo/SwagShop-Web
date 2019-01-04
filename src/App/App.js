da4043b9ba1f84ecfaaa8e32041a7b65c6d491d4
Andreas Velounias <andreas.velounias@rokigroup.co.uk>
2019-01-04 17:48:25 +0000

    Show products

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Product from '../Product/product';
 import HttpService from '../Services/http-service';
 
 const http = new HttpService();
@@ -9,17 +10,30 @@ class App extends Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {products: []};
     // Bind functions
     this.loadData = this.loadData.bind(this);
     this.loadData();
+    this.productList = this.productList.bind(this);
   }
 
   loadData = () => {
-    http.getProducts().then(products => {
-      console.log(products);
+    var self = this;
+    http.getProducts().then(data => {
+      self.setState({products: data.products})
     }, err => {
       console.log(err);
     }); 
+  }
+
+  productList = () => {
+    const list = this.state.products.map((product) =>
+    <div className="col-sm-4" key={product._id}>
+      <Product price={product.price}  title={product.title}  imgUrl={product.imgUrl}/>
+    </div>
+  );
+  return (list);
   }
 
   render() {
@@ -28,18 +42,12 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to The Swag Shop</h2>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <div className="container App-main">
+          <div className="row">
+            {this.productList()}
+          </div>
+        </div>
       </div>
     );
   }
